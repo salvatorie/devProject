bad01ea90c9b857e1912ace8b17f2591da041663
salvatorie <ejs6233@psu.edu>
2020-10-08 13:23:27 -0400

    Changed text to show how I feel about all this

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-        return "Hello World and Elliott"
+        return "Fuck this Git shit I have never been more confused in my life"
 
 if __name__ == "__main__":
     app.run()
